79e4bb16f55ecc4346a98c7d4e72414e681e96f9
Flavien <Debflav@hotmail.com>
2014-03-11 09:45:55 +0100

    Override FosUser RegistrationController

---
+++ src/Override/FosUserBundle/Controller/RegistrationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Override\FosUserBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+
+class RegistrationController extends BaseController
+{
+
+    public function registerAction(Request $request)
+    {
+        $securityService = $this->container->get('security.context');
+        if ($securityService->isGranted('ROLE_SECRETARY') ||
+            $securityService->isGranted('ROLE_ADMIN'))
+        {
+
+            return parent::registerAction($request);
+        }
+
+        throw new \Exception();
+    }
+}
