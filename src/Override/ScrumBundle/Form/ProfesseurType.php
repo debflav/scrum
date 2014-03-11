53cffb393dd11274ec012d738e7429d25f6ce0ae
Lyokoï <lyokoi@free.fr>
2014-03-11 13:40:51 +0100

    crud user

---
+++ src/Override/ScrumBundle/Form/ProfesseurType.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Override\ScrumBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ProfesseurType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('usernameCanonical')
+            ->add('email')
+            ->add('emailCanonical')
+            ->add('enabled')
+            ->add('salt')
+            ->add('password')
+            ->add('lastLogin')
+            ->add('locked')
+            ->add('expired')
+            ->add('expiresAt')
+            ->add('confirmationToken')
+            ->add('passwordRequestedAt')
+            ->add('roles')
+            ->add('credentialsExpired')
+            ->add('credentialsExpireAt')
+            ->add('nom')
+            ->add('prenom')
+            ->add('dateNaissance')
+            ->add('telephone')
+            ->add('adresse')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Override\ScrumBundle\Entity\Professeur'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'override_scrumbundle_professeur';
+    }
+}
