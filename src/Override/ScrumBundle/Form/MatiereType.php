994be88ed6581b23ea2760688f45f5a2f89b3fd0
Michel Bashares <michel.bashares@gmail.com>
2014-03-11 12:21:25 +0100

    CRUD Matiere

---
+++ src/Override/ScrumBundle/Form/MatiereType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Override\ScrumBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MatiereType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('coefficient')
+            ->add('description')
+            ->add('cursus')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Override\ScrumBundle\Entity\Matiere'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'override_scrumbundle_matiere';
+    }
+}
