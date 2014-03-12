28a7b585dc5793fbbfa75bdb383330f614039ac3
vagrant <vagrant@precise32.(none)>
2014-03-12 13:16:12 +0000

    Default login for users

--- src/Override/ScrumBundle/Controller/UserController.php
+++ src/Override/ScrumBundle/Controller/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
     /**
      * Lists all User entities.
      *
-     * @Secure(roles="{ROLE_ADMIN, ROLE_SECRETARY}")
+     * @Secure(roles="ROLE_ADMIN, ROLE_SECRETARY")
      * @Route("/", name="user")
      * @Method("GET")
      * @Template()
